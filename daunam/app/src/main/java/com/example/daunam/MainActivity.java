c834d50aff99ca1ae6bcc27cdc7a04baaa0c0622
LyLy123456789 <ntly.20it6@vku.udn.vn>
2022-05-19 16:33:40 +0700

    ...

---
+++ daunam/app/src/main/java/com/example/daunam/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.daunam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Thi Ly ", "Xin chao cac ban");
+        Log.d("Thi Ly ", "Xin chao cac ban");
+    }
+}
